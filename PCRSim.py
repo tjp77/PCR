3d7ea9f4b19252b64712e0accb8b50b693142c80
Tiffany Pankuch <31459605+tjp77@users.noreply.github.com>
2019-02-11 14:39:52 -0500

    Update PCRSim.py

--- PCRSim.py
+++ PCRSim.py
@@ -46,17 +46,50 @@ d = 200; e = 50;
 primerDecayOn = True; 
 
 
-# ...waiting on response about part of instructions to see if need to keep complimentary strands matched, or if will be fine to 
-# have indivdual unmatched strands represented, which I think would be a bit easier due to how python handles strings. 
-# [Confirmed, fragmentCount = indivdual single sided stands. No reason to represent as pairs, unless want more accurate/expandable representaton.]
+def buildComplimentaryStrand(strand):
+    
+    lowerStrand = strand.lower();
+    tmp1 = lowerStrand.replace("a", "T");
+    tmp2 = tmp1.replace("t", "A");
+    tmp3 = tmp2.replace("g", "C");
+    tmp4 = tmp3.replace("c", "G");
+    
+    return tmp4;
+    
+     
+class DNA: 
 
-def generateDNA(): 
+    # waiting on response about somehting to see if need to keep complimentary strands matched, or if will be fine to 
+    # have indivdual unmatched strands represented, which I think would be a bit easier due to how python handles strings. 
+    # [Confirmed, fragmentCount = indivdual single sided stands. No reason to represent as pairs, unless want more accurate/expandable representaton.]
     
-    dna = "x";
+    strand2Set = False;
     
+    def __init__(self, strand1, generateStrand2):
+        
+        self.strand1 = strand1;
+        
+        if (generateStrand2 == True):
+            self.strand2 = buildComplimentaryStrand(strand1);
+            self.strand2Set = True;
+        
+        def setStrand2(self, strand):
+            self.strand2 = strand;
+            self.strand2Set = True;
+            
+
+
+def generateDNA():  # Finish. Change to actually generate a long enough first strand. Test with this first, or comment out new code till tested. 
+    
+    strand1 = "AGGCTTAAAGCCTGATGCACACCGATGACTAGGCTCTCATCGAGTAGCGATCGGCCTTAAATATCCGTGATCGATGACGTACGTACTGACTGACTGTACTTAATCGTACTTCGAGCTAGTCGATGCATCGAGTTAGGCCCCTAGTCGATCTGATCGGTACGT";
+    
+    dna = DNA(strand1, True);
+
     return dna;
     
-def readInDna(fileName, isDoubleStrand, generateStrand2): 
+    
+    
+def readInDna(fileName, isDoubleStrand, generateStrand2): # Finish. 
     
     dna = "x";
     
@@ -64,17 +97,20 @@ def readInDna(fileName, isDoubleStrand, generateStrand2):
 
 
 def Step1(): 
+
     # In the first step, the two strands of the DNA are physically separated.
     
-    return 1; # 
-
-
+    return 1; 
+    
+    
 # Step 2 function. Primers bind to DNA.
+
 def Step2(dnaContainer, primerFrw, primerBkw):
     
-    # Search strands for primer binding locations (regex), if find a matching spot then can attach. 
-    # How to use variables in regex patterns: 
-    # https://stackoverflow.com/questions/6930982/how-to-use-a-variable-inside-a-regular-expression
+    # search strands for primer binding locations (regex), if find a matching spot then can attach. 
+	
+	#most of code for htis function written with being able to compile, will check when I get home, but at least serves as a base to go off of.
+	# (and still needing to decide to to represent strands, singly or doubly.)
     
     for strand in dnaContainer:
         
@@ -91,18 +127,27 @@ def Step2(dnaContainer, primerFrw, primerBkw):
             
             copyLen = mpSegLen;
         
-        # If regex find frw primer, build new strand made up of fwd primer + complimetary dna strand of area to be copied in front of frw primer.
+        # How to use variables in regex patterns: https://stackoverflow.com/questions/6930982/how-to-use-a-variable-inside-a-regular-expression
         
-        # elif regex find bkw primer, build new strand made up of bkw primer + complimetary dna strand of area to be copied behind bkw primer.
+		frwSearch = strand.find(primerFrw);
+		bwkSearch = strand.find(primerBkw);
+		
+        if frwSearch is not -1:
+            # build new strand made up of fwd primer + complimetary dna strand of area to be copied in front of frw primer.
+			copy = primerFrw + buildComplimentaryStrand(strand[frwSearch + primerLen : copyLen]);
         
-        # else incomeplete strand, likely due to previous early primer decay, strand does not get copied.  
+        elif bkwSearch is not -1:
+            # build new strand made up of bkw primer + complimetary dna strand of area to be copied behind bkw primer.
+			copy = primerBkw + buildComplimentaryStrand(strand[bkwSearch \: copyLen]);
+        else 
+            # incomeplete strand, likely due to previous early primer decay, strand does not get copied. 
         
         
-                
         # !!! Put new strands in new container at first. Not sure if python lets the iterated container be added to, 
         # and don't want them copied till next cycle even if so!
     
     return 1;
+
 
 # Step 3 function. Build copy off of primer, based on DNA.
 
@@ -111,8 +156,11 @@ def Step2(dnaContainer, primerFrw, primerBkw):
 
 def Step3():
     
+    # If length of area to copy is longer than strand section to be copied, will get a partial strand of shorter length. 
+    
     # Generate random fall off rate with: random.randint(-e, e) + d for primer of current strand being copied. 
     # Fall-off rate means that primer falls off after copying that many bases (A, T, G, C). [Confirmed]
+    
 
     return 1;
 
@@ -134,7 +182,9 @@ def displayLenDistribution(dnaContainer):
 def ResultPrint(dnaContainer):
     
     # Your output: (What you might see on the gel)
-    # 1. Statistics of the PCR products
+    
+    # 1. Statistics of the PCR products:
+    
     print ("Fragment Count: ", fragmentCount, "\n\n");
     print (computeAveStrandLen(dnaContainer), "\n\n"); 
     displayLenDistribution(dnaContainer) 
@@ -144,8 +194,6 @@ def ResultPrint(dnaContainer):
     return 0;
 
 
-# ----- Turn off the primer decay/fall off at first to check if getting the right amount of copied by the end for testing purposes. 
-
 def main():
     
     dnaContainer = []; # List to hold either strand pairs or individual strands depending how output needs and how we choose to represent. 
@@ -153,39 +201,40 @@ def main():
     # Get dna input sequence.
     dnaContainer.append(generateDNA());
 
-    # Get input of what each primer should be so can choose region to copy. 
-    # -------- Test with premade ones here first, then add in input options. 
-    # Validate input that both entered primers are the same length, update common primer length var defined above.
-        
-    primerFrw = "x";
-    primerBkw = "x";
+    # Get input of what each primer should be so can choose region to copy. -------- Test with premade ones here first, then add in input options. 
+    # Validate input that both entered primers are the same length, update common primer length var defined above. 
+    
+    primerFrw = "AGGCTTAAAGCCTGATGCAC"; 
+    primerBkw = "TCAGCTAGACTAGCCATGCA";
+    
+    ampSegLen = int(input("What is the length of the segment you wish to copy?"));
     
     # Get cycle count input From user. 
     cycleCount = int(input("How many cycles should be ran?\n")); 
     completeCycles = 0;
     
-    # Ask user if want to turn on primer decay.
+    # Ask user if want to turn on primer decay. 
+    
+    # If for any step where, for our purposes, changing the step time length would make a difference, allow time input.
     
     while completeCycles < cycleCount:
         
-        # Don't have physical strands to separate, unless decide to still represent them as pairs, may not need distinct step 1?
-        Step1(); 
-        # Maybe easier to combine steps 2 ^ 3 ^ just attach primer ^ build in one step from code perspective, one string.
-        Step2(dnaContainer, primerFrw, primerBkw); 
+        Step1(); # Don't have physical strands to separate, unless decide to still represent them as pairs, may not need distinct step 1. 
+        Step2(dnaContainer, primerFrw, primerBkw); # Maybe easier to combine steps 2^3 ^ just attach primer ^ build in one step from code perspective, one string.
         Step3();
         completeCycles += 1;
         
         # Print out results of each cycle, at least for checking program correctness and debugging purposes if nothing else? 
     
-    # Adding in base display structure now so nothing forgotten about later. 
     ResultPrint(dnaContainer);
     
-    # "You will get bonus points if it you can add more parameters and some limitations (such as amount of primers, dNTPs, age of 
-    # taqs, temperature, mutations, …)"
+    # You will get bonus points if it you can add more parameters and some limitations (such as amount of primers, dNTPs, age of 
+    # taqs, temperature, mutations, …)
     
     # Adding a chance for mutations which can be turned on and off seems like a good/simple one to do. 
     
     return 0; 
+
 
 
 
